5efcd96f1ae5ddb8e78e297c3a1b5e9514e0b158
Clowneli <106204349+Clowneli@users.noreply.github.com>
2024-02-16 14:21:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My parents healthy new diner')
+streamlit.title('My Parents Healthy New Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+stramlit.text('Hard-Boiled Free-Range Egg')
